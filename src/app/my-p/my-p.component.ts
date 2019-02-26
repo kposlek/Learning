916be94ba56d676365e8a7c6fdea937395ed99b3
Karolina Poslek <karolina@poslek.com>
2019-02-26 23:47:43 +0100

    Kalkulator

--- src/app/my-p/my-p.component.ts
+++ src/app/my-p/my-p.component.ts
@@ -9,4 +9,28 @@ export class MyPComponent {
   name: string;
   @Input()
   bla: string;
+  @Input()
+  foo: string = 'joo';
+
+  title: string = 'nesto';
+  prviBroj: number = 0;
+  drugiBroj: number = 0;
+  zbroj: number = 0;
+
+  maca() {
+    console.log('Ja sam mica maca... tralalaaalaaaa');
+  }
+  tipka(event) {
+    this.title = event.target.value;
+  }
+  setPrviBroj(event) {
+    this.prviBroj = parseInt(event.target.value);
+  }
+  setDrugiBroj(event) {
+    this.drugiBroj = parseInt(event.target.value);
+  }
+
+  zbroji() {
+    this.zbroj = this.prviBroj + this.drugiBroj;
+  }
 }
